b7e72fe037306a109d1db4d5fa6a63c264dd645a
daws11 <dawskutel@gmail.com>
2023-06-09 03:51:20 +0700

    update setting

--- lib/Screens/account/akunPage.dart
+++ lib/Screens/account/akunPage.dart
@@ -1,193 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:login/Screens/account/akunPage.dart';
-import 'package:login/Screens/account/editProfilPage.dart';
-import 'package:login/Screens/homepage/components/home_page_body.dart';
-import 'package:login/Screens/article/artikelPage.dart';
-import 'package:provider/provider.dart';
-import 'package:login/models/artikelProvider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AkunPage extends StatelessWidget {
-  const AkunPage({Key? key}) : super(key: key);
+class AkunPage extends StatefulWidget {
+  @override
+  _AkunPageState createState() => _AkunPageState();
+}
+
+class _AkunPageState extends State<AkunPage> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late User user;
+  String? username, email, mobileNumber, userType, collegeName;
+
+  @override
+  void initState() {
+    super.initState();
+    user = auth.currentUser!;
+    _loadUserProfile();
+  }
+
+  Future<void> _loadUserProfile() async {
+    var docSnapshot = await firestore.collection("users").doc(user.uid).get();
+    if (docSnapshot.exists) {
+      var userData = docSnapshot.data();
+      setState(() {
+        username = userData!["username"];
+        email = userData["email"];
+        mobileNumber = userData["mobileNumber"];
+        userType = userData["userType"];
+        collegeName = userData["collegeName"];
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        flexibleSpace: Container(
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(12),
-                bottomRight: Radius.circular(12)),
-            gradient:
-                LinearGradient(colors: [Colors.green, Colors.greenAccent]),
-          ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back), // Add this line to add back button
+          onPressed: () {
+            Navigator.pop(context); // Add this line to make back button work
+          },
         ),
-        title: Row(
-          mainAxisAlignment:
-              MainAxisAlignment.spaceBetween, // memberi spasi antar widget
-          children: [
-            Icon(Icons.settings_outlined, size: 40),
-            Text('Settings'),
-            SizedBox(
-              width: 250,
-            ),
-          ],
-        ),
-      ),
-      body: ListView(
-        padding: EdgeInsets.only(top: 20),
-        children: [
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Edit Profile'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Editprofile()));
-              // showDialog(
-              //   context: context,
-              //   builder: (BuildContext context) {
-              //     return AlertDialog(
-              //       title: Text('Edit Profile'),
-              //       content: Text('Here you can change your app settings.'),
-              //       actions: <Widget>[
-              //         TextButton(
-              //           child: Text('CANCEL'),
-              //           onPressed: () {
-              //             Navigator.of(context).pop();
-              //           },
-              //         ),
-              //         TextButton(
-              //           child: Text('OK'),
-              //           onPressed: () {
-              //             // Perform the desired action on OK button press
-              //             Navigator.of(context).pop();
-              //           },
-              //         ),
-              //       ],
-              //     );
-              //   },
-              // );
+        title: Text('Settings'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              // Put the function to handle settings here
             },
-          ),
-          ListTile(
-            leading: Icon(Icons.language),
-            title: Text('Language'),
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text('Language'),
-                    content: Text('Here you can change your app settings.'),
-                    actions: <Widget>[
-                      TextButton(
-                        child: Text('CANCEL'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      TextButton(
-                        child: Text('OK'),
-                        onPressed: () {
-                          // Perform the desired action on OK button press
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.bookmark),
-            title: Text('Bookmark'),
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text('Bookmark'),
-                    content: Text('Here you can change your app settings.'),
-                    actions: <Widget>[
-                      TextButton(
-                        child: Text('CANCEL'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      TextButton(
-                        child: Text('OK'),
-                        onPressed: () {
-                          // Perform the desired action on OK button press
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-          ),
+          )
         ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("User Name"), // Replace with user name
-              accountEmail: Text("User Status"), // Replace with user status
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.orange,
-                child: Text(
-                  "U", // Replace with the first letter of the user name
-                  style: TextStyle(fontSize: 40.0),
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomeScreenBody()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.bookmark),
-              title: Text('Bookmark'),
-              onTap: () {
-                //fungsi bookmark
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => AkunPage()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('Log Out'),
-              onTap: () {
-                //fungsi log out firebase
-              },
-            ),
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text('Username'),
+            subtitle: Text(username ?? 'Loading...'),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text('Email'),
+            subtitle: Text(email ?? 'Loading...'),
+          ),
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('Mobile Number'),
+            subtitle: Text(mobileNumber ?? 'Loading...'),
+          ),
+          ListTile(
+            leading: Icon(Icons.school),
+            title: Text('College Name'),
+            subtitle: Text(collegeName ?? 'Loading...'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('User Type'),
+            subtitle: Text(userType ?? 'Loading...'),
+          ),
+        ],
       ),
     );
   }
